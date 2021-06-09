572ff7300b98081145116b950156bc9754392f4c
Ved Mahajan <vsmahajan2k@gmail.com>
2021-06-09 20:44:59 +0530

    added Euler Method

---
+++ math/euler_method.jl
@@ -0,0 +1,21 @@
+"""
+Calculate the solution to a differential equation using forward euler method.
+"""
+
+using Test
+
+function euler_method(f, x0, span, h = 1.0e-2)
+    s,e = span
+    steps = floor((e-s)/h) |> Int
+    x = Vector{Float64}(undef,steps)
+	t = Vector{Float64}(undef,steps)
+    x[1] = x0
+	t[1] = 0
+    for i in 1:(steps-1)
+        x[i+1] = x[i] + h*f(x[i],t[i])
+		t[i+1] = t[i] + h
+    end
+    return x,t
+end
+
+@test euler_method((x,t) -> x,1,(0,5))[1][end] == 143.33937864611195
