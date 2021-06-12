91cb553347956e2e6f5f26a8f44203030d4e328d
Madison Estabrook <madison.estabrook@gmail.com>
2021-06-12 13:16:43 -0400

    Add is_palindrome
    Added  is_palindrome.jl under the src > string folder.

---
+++ src/strings/is_palindrome.jl
@@ -0,0 +1,13 @@
+#=
+A set of functions to check if a given string
+is a palindrome.
+
+Arguments:
+ - s - a string 
+=#
+
+function is_palindrome(s) # Named function
+    s == reverse(s)
+end
+
+s -> s == reverse(s) # Anonymous
