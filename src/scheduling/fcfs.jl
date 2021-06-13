b58472986e98814362f84579d380d87e06693c39
Ashwani Rathee <ab669522@gmail.com>
2021-06-13 19:07:08 +0530

    First come first serve

---
+++ src/scheduling/fcfs.jl
@@ -0,0 +1,44 @@
+"""
+    fcfs()
+
+Implementation of first come first served scheduling algorithm
+
+# Example
+
+```julia
+n = 3
+process_id = Any[1, 2, 3]
+burst_times = Any[3, 4, 5]
+fcfs(n, process_id, burst_times)
+
+```
+# Reference
+
+https://en.wikipedia.org/wiki/Scheduling_(computing)#First_come,_first_served
+"""
+function fcfs(n, process_id, burst_time)
+    waiting_time = []
+    push!(waiting_time, 0)
+    for i = 2:n
+        # Calculates waiting time
+        push!(waiting_time, burst_time[i-1] + waiting_time[i-1])
+    end
+
+    turnaround_time = []
+    for i = 1:n
+        # Calculates turnaround time
+        push!(turnaround_time, burst_time[i] + waiting_time[i])
+    end
+    
+    # Calculates Average waiting time
+    avg_waiting_time = sum(waiting_time) / length(waiting_time)
+
+    # Calculates Average turnaround time
+    avg_turnaround_time = sum(turnaround_time) / length(turnaround_time)
+
+    return process_id, burst_time, waiting_time, turnaround_time, avg_waiting_time, avg_turnaround_time
+end
+n = 3
+process_id = Any[1, 2, 3]
+burst_times = Any[10, 5, 8]
+fcfs(n, process_id, burst_times)
