7ac5b39b8979c27aa65f3151130c4d4f8a9927fb
Rratic <73381027+Rratic@users.noreply.github.com>
2021-06-14 20:14:18 +0800

    add data structures (#48)

--- src/TheAlgorithms.jl
+++ src/TheAlgorithms.jl
@@ -12,6 +12,10 @@ using Random
 ## Exports
 # Please keep the folders/functions sorted
 
+# Exports: data_structures
+export AbstractBinaryTree,AbstractBinaryTree_arr,BinaryTree,ch,isleaf,height,left,right,insert!,depth
+export DisjointSet,find,merge!
+
 # Exports: knapsack
 export ZeroOnePack!
 export CompletePack!
@@ -73,6 +77,10 @@ export fcfs
 
 ## Includes
 # Please keep the folders/files sorted (by dependencies then alphabetical order)
+
+# Includes: data_structures
+include("data_structures/binary_tree/basic_binary_tree.jl")
+include("data_structures/disjoint_set/disjoint_set.jl")
 
 # Includes: knapsack
 include("knapsack/knapsack.jl")
