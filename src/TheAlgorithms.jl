5de2a3117592ea3928f1b046b6d540f3ead6ece1
Ved Mahajan <vsmahajan2k@gmail.com>
2021-06-14 17:47:29 +0530

    corrected the name

--- src/TheAlgorithms.jl
+++ src/TheAlgorithms.jl
@@ -95,7 +95,7 @@ include("math/sum_of_geometric_progression.jl")
 # Includes: matrix
 include("matrix/lu_decompose.jl") # used by determinant.jl
 include("matrix/determinant.jl")
-include("matrix/rotation_matrix.jl")
+include("matrix/rotation-matrix.jl")
 
 # Includes: project-rosalind
 include("project-rosalind/count_nucleotide.jl")
