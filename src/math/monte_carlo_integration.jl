77f8a24b9b57240a44b179c58657f25d3ff59758
Ved Mahajan <vsmahajan2k@gmail.com>
2021-06-16 21:46:09 +0530

    added monte carlo integration

---
+++ src/math/monte_carlo_integration.jl
@@ -0,0 +1,16 @@
+"""
+Monte carlo integration is a very easy and scalable way to do multidimentional integrals.
+However, only single variable integrals is considered.
+
+Refereces:
+-https://kingaa.github.io/sbied/pfilter/monteCarlo.html
+-https://kingaa.github.io/sbied/pfilter/monteCarlo.html
+
+Contributed by: [Ved Mahajan](https://github.com/Ved-Mahajan)
+"""
+function monte_carlo_integration(f,N,span)
+	x_start,x_end = span
+	rand_array = rand(x_start:0.0001:x_end,N)
+	return sum(f.(rand_array))*(x_end - x_start)/float(N)
+end
+
--- test/math.jl
+++ test/math.jl
@@ -147,6 +147,12 @@
         @test_throws ErrorException  factorial_recursive(-1)
     end
 
+	@testset "Math: Monte Carlo Integration" begin
+		@test isapprox(monte_carlo_integration(x->3*x^2,100000,(0,1)),1,atol = 0.01)
+		@test isapprox(monte_carlo_integration(x->sin(x),1000,(0,pi)),2,atol = 0.1)
+	end
+		
+
     @testset "Math: Prime Check" begin
         @test prime_check(2) == true
         @test prime_check(3) == true
