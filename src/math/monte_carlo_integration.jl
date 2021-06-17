9374b517a638cd932463e061f1a54f2900369c78
Ved Mahajan <74182892+Ved-Mahajan@users.noreply.github.com>
2021-06-17 13:33:48 +0530

    Update src/math/monte_carlo_integration.jl
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- src/math/monte_carlo_integration.jl
+++ src/math/monte_carlo_integration.jl
@@ -6,11 +6,10 @@ Refereces:
 -https://kingaa.github.io/sbied/pfilter/monteCarlo.html
 -https://kingaa.github.io/sbied/pfilter/monteCarlo.html
 
-Contributed by: [Ved Mahajan](https://github.com/Ved-Mahajan)
+Contributed By-: [Ved Mahajan](https://github.com/Ved-Mahajan)
 """
 function monte_carlo_integration(f,N,span)
 	x_start,x_end = span
 	rand_array = rand(x_start:0.0001:x_end,N)
 	return sum(f.(rand_array))*(x_end - x_start)/float(N)
 end
-
