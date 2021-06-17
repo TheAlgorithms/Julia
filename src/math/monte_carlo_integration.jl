4932af5957d88c8ebea4c5213aa95a9cd94d8ee6
Ved Mahajan <vsmahajan2k@gmail.com>
2021-06-17 13:38:36 +0530

    added documentation

--- src/math/monte_carlo_integration.jl
+++ src/math/monte_carlo_integration.jl
@@ -2,8 +2,14 @@
 Monte carlo integration is a very easy and scalable way to do multidimentional integrals.
 However, only single variable integrals is considered.
 
+
+This function takes three outputs:
+	- `f`: the function to integrate. (at the momment only single variable is suported)
+	- `N`: Number of points to sample. For most simple functions, 1000 to 10,000 should be okay.
+	- `span`: a tuple (start, end) for the integration limits
 Refereces:
--https://kingaa.github.io/sbied/pfilter/monteCarlo.html
+
+-https://www.scratchapixel.com/lessons/mathematics-physics-for-computer-graphics/monte-carlo-methods-in-practice/monte-carlo-integration
 -https://kingaa.github.io/sbied/pfilter/monteCarlo.html
 
 Contributed by: [Ved Mahajan](https://github.com/Ved-Mahajan)
