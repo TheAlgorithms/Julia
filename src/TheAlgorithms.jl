65ca2e3ce605ae44fa9854b4dd95f7748e992d06
ErikQQY <2283984853@qq.com>
2021-06-21 15:39:57 +0800

    Add Volume computing methods
    Signed-off-by: ErikQQY <2283984853@qq.com>
    
    Fix failing checks
    
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/TheAlgorithms.jl
+++ src/TheAlgorithms.jl
@@ -66,6 +66,14 @@ export sum_gp
 export surfarea_cube
 export surfarea_sphere
 export trapazoidal_area
+export vol_cube
+export vol_cuboid
+export vol_cone
+export vol_right_circ_cone
+export vol_prism
+export vol_pyramid
+export vol_sphere
+export vol_circular_cylinder
 
 # Exports: matrix
 export determinant
@@ -138,6 +146,7 @@ include("math/perfect_square.jl")
 include("math/sir_model.jl")
 include("math/sum_of_arithmetic_series.jl")
 include("math/sum_of_geometric_progression.jl")
+include("math/volume.jl")
 
 # Includes: matrix
 include("matrix/lu_decompose.jl") # used by determinant.jl
---
+++ src/math/volume.jl
@@ -0,0 +1,153 @@
+#Find Volumes of Various Shapes.
+#Wikipedia reference: https://en.wikipedia.org/wiki/Volume
+# contributed By:- [Qingyu Qu](https://github.com/ErikQQY)
+
+"""
+    vol_cube()
+
+Compute the volume of a cube.
+
+# Examples
+
+```julia-repl
+julia> vol_cube(1)
+1
+julia> vol_cube(3)
+27
+julia> vol_cube(-1)
+DomainError
+```
+"""
+function vol_cube(side)
+    if side<0
+        throw(DomainError(side, "vol_cube() only works for non negtive values."))
+    end
+    return side^3
+end
+
+"""
+    vol_cuboid(width, height, length)
+
+Compute the volume of a vol_cuboid
+
+# Examples
+
+```julia-repl
+julia> vol_cuboid(1, 1, 1)
+1
+julia> vol_cuboid(1, 2, 3)
+6
+```
+"""
+function vol_cuboid(width, height, length)
+    return width*height*length
+end
+
+"""
+    vol_cone(area_of_base, height)
+
+Compute the Volume of a Cone
+
+
+# Examples
+
+```julia-repl
+julia> vol_cone(10, 3)
+10.0
+julia> vol_cone(1, 1)
+0.3333333333333333
+```
+"""
+function vol_cone(area_of_base, height)
+    return area_of_base*height/3.0
+end
+
+"""
+    vol_right_circ_cone(radius, height)
+
+Compute the Volume of a Right Circular Cone.
+
+# Examples
+
+```julia-repl
+julia> vol_right_circ_cone(2, 3)
+12.566370614359172
+```
+"""
+function vol_right_circ_cone(radius, height)
+    return π*radius^2*height/3.0
+end
+
+"""
+    vol_prism(area_of_base, height)
+
+Compute the Volume of a Prism.
+
+# Examples
+
+```julia-repl
+julia> vol_prism(10, 2)
+20.0
+julia> vol_prism(11, 1)
+11.0
+```
+"""
+function vol_prism(area_of_base, height)
+    return area_of_base*height
+end
+
+"""
+    vol_pyramid(area_of_base, height)
+
+Compute the volume of a Pyramid.
+
+# Examples
+
+```julia-repl
+julia> vol_pyramid(10, 3)
+10.0
+julia> vol_pyramid(1.5, 3)
+1.5
+```
+"""
+function vol_pyramid(area_of_base, height)
+    return area_of_base*height/3.0
+end
+
+"""
+    vol_sphere(radius)
+
+Compute the volume of a sphere.
+
+# Examples
+
+```julia
+vol_sphere(5)  # returns 523.5987755982989
+vol_sphere(1)  # returns 4.1887902047863905
+vol_sphere(-1) # returns DomainError
+```
+"""
+function vol_sphere(radius)
+    if radius < 0
+        throw(DomainError)
+    end
+    return 4/3π*(radius)^3
+end
+
+"""
+    vol_circular_cylinder(radius, height)
+
+Compute the Volume of a Circular Cylinder.
+
+# Examples
+
+```julia-repl
+julia> vol_circular_cylinder(1, 1)
+3.141592653589793
+julia> vol_circular_cylinder(4, 3)
+150.79644737231007
+```
+"""
+function vol_circular_cylinder(area_of_base, height)
+    return pi*radius^2*height
+end
