67f682229a7c1233f45c4ce90a906c6988488649
Soc Virnyl S. Estela <66054069+ReneganRonin@users.noreply.github.com>
2021-06-30 16:44:40 +0800

    Add documentation variance

--- src/statistics/variance.jl
+++ src/statistics/variance.jl
@@ -1,8 +1,29 @@
-# TODO: doc is missing
+"""
+    variance(a::Vector{T}) where T <: Number
+    variance(a::UnitRange{T}) where T <: Number
 
-function variance(a)
+Find the variance from a set of data.
+
+# Arguments:
+- `a`: holds the set of data
+
+# Reference
+    - According to Ronald E. Walpole, `variance` is used to measure the variability of a set of data. -- Introduction to Statistics by Ronald E. Walpole
+
+# Contributors:
+- [Aru Bhardwaj](https://github.com/arubhardwaj)
+"""
+function variance(a::Vector{T}) where T <: Number
     avg = a / length(a)
     x = sum(a - avg)
     s_sq = x / (length(a) - 1)
     return(s_sq)
 end
+
+function variance(a::UnitRange{T}) where T <: Number
+    avg = a / length(a)
+    x = sum(a - avg)
+    s_sq = x / (length(a) - 1)
+    return(s_sq)
+end
+
