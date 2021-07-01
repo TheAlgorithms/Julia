f268671a6179a663ef02756ce55778531a8f4984
Ashwani Rathee <ab669522@gmail.com>
2021-07-01 16:43:24 +0530

    krishnamurthy number (#58)
    * krishnamurthy number
    
    * Fixes
    
    * Fix indent in docs
    
    * krishnamurthy number
    
    * Fixes
    
    * Fix indent in docs
    
    Co-authored-by: Jean-Francois Baffier <jf@baffier.fr>

--- test/math.jl
+++ test/math.jl
@@ -146,11 +146,18 @@
         @test_throws ErrorException  factorial_recursive(-1)
     end
 
+    @testset "Math: Krishnamurthy Number" begin
+        @test krishnamurthy(145) == true
+        @test krishnamurthy(240) == false
+        @test krishnamurthy(1) == true
+        @test_throws ErrorException krishnamurthy(0.1)
+        @test_throws ErrorException krishnamurthy(-1)
+    end
+
 	  @testset "Math: Monte Carlo Integration" begin
 		    @test isapprox(monte_carlo_integration(x->3*x^2,100000,(0,1)),1,atol = 0.01)
 		    @test isapprox(monte_carlo_integration(x->sin(x),1000,(0,pi)),2,atol = 0.1)
 	  end
-		
 
     @testset "Math: Prime Check" begin
         @test prime_check(2) == true
@@ -234,8 +241,8 @@
         @test eratosthenes(2) == [2]
         @test eratosthenes(1) == Int64[] # No prime number less that 1, so it should return empty array.
     end
-    
+
    @testset "Math: Verlet scheme" begin
-        @test verlet_integration(x->-x,[0.0,0.1],(1,10))[end][end] == 9.999	
+        @test verlet_integration(x->-x,[0.0,0.1],(1,10))[end][end] == 9.999
    end
 end
