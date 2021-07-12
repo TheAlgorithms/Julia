aa321f66cade905da965c36b0066ed266376aa41
Mayur Dahibhate <52608503+mayurdahibhate@users.noreply.github.com>
2021-07-12 22:26:43 +0530

    add combinations and permutations (#69)
    * add combinations and permutations
    
    * Apply suggestions from code review
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    
    * included files in main source file
    
    * fix type conversion
    
    * fix and rearrange tests
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

---
+++ src/math/permutation.jl
@@ -0,0 +1,33 @@
+"""
+    permutation(n, r)
+        
+    returns the number of ways to choose r items from n items
+    with order and without repetition
+
+# Arguments:
+- `n`: Positive integers of items to choose from
+- 'r': Positive integers of items to choose
+
+Contributed By:- [Mayur Dahibhate](https://github.com/mayurdahibhate)
+"""
+
+function permutation(n, r)
+
+    function factorial(n)        
+        fact = 1
+        
+        if n == 0 || n == 1
+            return fact
+        end
+         
+        for i = 1:n
+            fact = fact * i
+        end 
+        
+        return fact
+    end
+    
+    perm = factorial(n) / factorial(n - r) 
+
+    return convert(Int64, perm)
+end
