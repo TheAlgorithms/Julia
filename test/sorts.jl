4e44de8c5663ed2bc91d95ff42af63eb2b89f8e6
Jean-Francois Baffier <jf@baffier.fr>
2021-07-22 09:02:29 +0900

    Update sorts.jl

--- test/sorts.jl
+++ test/sorts.jl
@@ -1,20 +1,16 @@
 @testset "Sorts" begin
-    x=[3,5,1,4,2]
-    BubbleSort!(x)
-    @test x == [1,2,3,4,5]
-    x=[3,5,1,4,2]
-    InsertionSort!(x)
-    @test x == [1,2,3,4,5]
-    x=[3,5,1,4,2]
-    SelectionSort!(x)
-    @test x == [1,2,3,4,5]
-    x=[3,5,1,4,2]
-    MergeSort!(x)
-    @test x == [1,2,3,4,5]
-    x=[3,5,1,4,2]
-    QuickSort!(x)
-    @test x == [1,2,3,4,5]
-    x=[3,5,1,4,2]
-    ExchangeSort!(x)
-    @test x == [1,2,3,4,5]
+    y = [1,2,3,4,5]
+    
+    sorts = [
+        BubbleSort!,
+        ExchangeSort!
+        InsertionSort!,
+        MergeSort!,
+        QuickSort!,
+        SelectionSort!,
+    ]
+    
+    for f in sorts
+        @test f([3,5,1,4,2]) == y
+    end
 end
