1c3fc353a55ed28ae98a30428c79203fdf5196a5
Soc Virnyl S. Estela <66054069+uncomfyhalomacro@users.noreply.github.com>
2021-08-07 17:08:27 +0800

    Add contributor

--- src/searches/exponential_search.jl
+++ src/searches/exponential_search.jl
@@ -13,6 +13,8 @@ O(Log n)
 Applications of Exponential Search:
 Exponential Binary Search is particularly useful for unbounded searches, where size of array is infinite. Please refer Unbounded Binary Search for an example.
 It works better than Binary Search for bounded arrays, and also when the element to be searched is closer to the first element.
+
+Contributed By:- [Ash](https://github.com/ashwani-rathee)
 """
 function exponential_search(arr::AbstractArray{T,1}, x::T) where {T<:Real}
     n = size(arr)[1]
