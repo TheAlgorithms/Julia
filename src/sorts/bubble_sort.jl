f804fb95f83876da2f017a0d4b98446e19afa061
hattori-emi <a7b8i06c49@outlook.com>
2021-08-20 18:25:04 +0800

    Re implementation bubble sort

--- src/sorts/bubble_sort.jl
+++ src/sorts/bubble_sort.jl
@@ -1,6 +1,6 @@
 function bubble_sort!(array::Vector{T})where T
     for i = 1:length(array)
-    flag = false
+    flag = true
         for j = 1:(length(array) - i)
             if array[j] > array[j+1]
                 array[j], array[j + 1] = array[j + 1], array[j]
