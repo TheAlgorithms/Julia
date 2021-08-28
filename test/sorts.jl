871beb6491dafaa508aa2563ee21bb195d2b5d4a
Ming Liang <44579558+DrakonDarquesse@users.noreply.github.com>
2021-08-29 03:10:55 +0800

    added test cases of sorting algorithms (#2)
    * added test cases of sorting algorithms
    
    * added more test cases of sorting algorithms
    
    * improved test cases of sorting algorithms
    
    * fix test cases

--- test/sorts.jl
+++ test/sorts.jl
@@ -12,7 +12,20 @@
 
     for f in sorts
         x = [3,5,1,4,2]
+        y = [12,4,3,2,1]
+        empty = []
+        one = [1]
+
         f(x)
         @test x == [1,2,3,4,5]
+
+        f(y)
+        @test y == [1,2,3,4,12]
+        
+        f(empty)
+        @test empty == []
+        
+        f(one)
+        @test one == [1]
     end
 end
