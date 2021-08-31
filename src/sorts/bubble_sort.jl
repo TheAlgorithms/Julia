436a0b27c6efd376939e3af2c0295a47bd9f9041
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2021-08-31 17:56:14 +0800

    Update bubble_sort.jl
    formatted using JuliaFormatter

--- src/sorts/bubble_sort.jl
+++ src/sorts/bubble_sort.jl
@@ -1,12 +1,14 @@
-function bubble_sort!(array::Vector{T})where T
+function bubble_sort!(array::Vector{T}) where {T}
     for i = 1:length(array)
-    flag = false
-        for j = 1:(length(array) - i)
+        flag = false
+        for j = 1:(length(array)-i)
             if array[j] > array[j+1]
-                array[j], array[j + 1] = array[j + 1], array[j]
+                array[j], array[j+1] = array[j+1], array[j]
             end
         end
-        
-        if flag return end
+
+        if flag
+            return
+        end
     end
 end
