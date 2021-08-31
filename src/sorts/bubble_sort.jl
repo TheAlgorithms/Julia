48290c447498ae36bbc3a2ac7a92ba6649edf4fa
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2021-08-31 16:31:46 +0800

    Update bubble_sort.jl

--- src/sorts/bubble_sort.jl
+++ src/sorts/bubble_sort.jl
@@ -1,11 +1,17 @@
-function bubble_sort!(array::Vector{T})where T
-    for i = 1:length(array)
+function bubble_sort!(array::Vector{T}) where {T}
     flag = true
-        for j = 1:(length(array) - i)
-            if array[j] > array[j+1]
-                array[j], array[j + 1] = array[j + 1], array[j]
+    while flag
+        for i = 1:length(array)
+            flag = false
+            for j = 1:(length(array)-i)
+                if array[j] > array[j+1]
+                    array[j], array[j+1] = array[j+1], array[j]
+
+                end
+            end
+            if flag
+                return
             end
         end
-        if flag return end
     end
 end
