7caeb133c2907f10207752d396d52dd7d6dd1c19
Augusto C. Leal <leal.augusto@unifesp.br>
2021-10-08 20:45:57 -0300

    feat: Add trapezoid sum integration algorithm (#88)
    * add trapzoid sum integration algorithm
    
    * Add endline suggested from code review
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    
    * add endlines suggested in code review
    
    * make function exported in pkg
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- src/TheAlgorithms.jl
+++ src/TheAlgorithms.jl
@@ -75,6 +75,7 @@ export sum_gp
 export surfarea_cube
 export surfarea_sphere
 export trapazoidal_area
+export trapezoid_integration
 export verlet_integration
 export vol_cube
 export vol_cuboid
@@ -175,6 +176,7 @@ include("math/sieve_of_eratosthenes.jl")
 include("math/simpsons_integration.jl")
 include("math/sum_of_arithmetic_series.jl")
 include("math/sum_of_geometric_progression.jl")
+include("math/trapezoid_integration.jl")
 include("math/verlet.jl")
 include("math/volume.jl")
 
