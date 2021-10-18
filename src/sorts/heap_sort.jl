1a4261de273ca7ec583b802954065ea346d4a6f8
Frank Schmitt <frankschmitt@gmx.de>
2021-10-18 21:58:29 +0200

    add doc for heap_sort, and introduce param for comparison criterion

--- src/sorts/heap_sort.jl
+++ src/sorts/heap_sort.jl
@@ -1,10 +1,27 @@
+"""Simple helper function for converting 0-based indices to Julia's 1-based indices.
+"""
 function idx_for(i::Int) 
   return i + 1
 end
 
-"""Sort the given vector (in-place) using the Heapsort algorithm
+"""Sort the given vector (in-place) using the Heapsort algorithm.
+
+   Heapsort consists of two stages:
+    1. Building a (max) heap of the array
+    2. Repeatedly extracting the largest element and inserting it at the front of the sorted part of the array
+
+   After the largest element has been extracted, the tree is updated to maintain the heap property via a "sifting" operation.
+
+   Storing a heap in an array is pretty straightforward - for every node with index n, its children are stored at indices
+   2n + 1 and 2n + 2 (for 0-based indices). Index 0 contains the root node.
+   Since Julia's indices are 1-based, we need to change this a little bit. We're using a trivial helper function 
+   idx_for to convert from 0-based to 1-based.
+
+   See https://en.wikipedia.org/wiki/Heapsort for a complete explanation of Heapsort.
+
+   Contributed By:- [Frank Schmitt](https://github.com/frankschmitt)
 """ 
-function heap_sort!(arr::Vector{T}, N::Int=length(arr)) where {T}
+function heap_sort!(arr::Vector{T}, gt = >, N::Int = length(arr)) where {T}
       n = N 
       i = div(n,2) 
       t = -1
@@ -27,15 +44,15 @@ function heap_sort!(arr::Vector{T}, N::Int=length(arr)) where {T}
   
           # Sift operation - pushing the value of t down the heap 
           while (child < n) 
-              if (child + 1 < n  &&  arr[idx_for(child + 1)] > arr[idx_for(child)]) 
+              if (child + 1 < n  &&  gt(arr[idx_for(child + 1)], arr[idx_for(child)])) 
                   child += 1 # Choose the largest child
               end 
-              if (arr[idx_for(child)] > t) # If any child is bigger than the parent 
-                  arr[idx_for(parent)] = arr[idx_for(child)] # Move the largest child up 
-                  parent = child # Move parent pointer to this child 
-                  child = parent*2+1 #  find the next child"""
+              if (gt(arr[idx_for(child)], t))                   # If any child is bigger than the parent 
+                  arr[idx_for(parent)] = arr[idx_for(child)]    # Move the largest child up 
+                  parent = child                                # Move parent pointer to this child 
+                  child = parent*2+1                            #  find the next child
               else
-                  break # /* t's place is found */"""
+                  break # t's place is found 
               end
           end
           arr[idx_for(parent)] = t # We save t in the heap 
