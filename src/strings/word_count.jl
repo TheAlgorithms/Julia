45e2107882bbc92b3b6c441c3d6205aa3f7d5860
Ihjass Thasbekha <44056715+Ihjass@users.noreply.github.com>
2021-11-12 13:03:34 +0530

    Adding word count program (#101)
    * Adding word count program
    
    * Update word_count.jl
    
    * Update word_count.jl
    
    * Update src/strings/word_count.jl
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    
    * Update src/strings/word_count.jl
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    
    * Update strings.jl
    
    * Fixing test
    
    * Update word_count
    
    - export word_count into the module
    - included word_count.jl into the module
    - fix tests for word_count function
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    Co-authored-by: Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>

---
+++ src/strings/word_count.jl
@@ -0,0 +1,53 @@
+"""
+wordcount(sentence)
+
+Program to find word count in the given sentence. The program will return count with the word.
+A full description of the algorithm can be found on [exercism](https://exercism.org/tracks/julia/exercises/word-count)
+
+# Arguments:
+- `sentence`: The sentence to find the word count.
+
+# Examples/Tests 
+```julia
+julia> wordcount(The quick brown fox jumps over the lazy dog)
+Dict{Any, Any}("jumps" => 1, "the" => 2, "brown" => 1, "over" => 1, "quick" => 1, "lazy" => 1, "dog" => 1, "fox" => 1)
+
+julia> wordcount(the sky is blue and beautiful)
+Dict{Any, Any}("and" => 1, "the" => 1, "sky" => 1, "blue" => 1, "is" => 1, "beautiful" => 1)
+
+```
+
+# Algorithm: 
+
+```julia
+
+for word in eachmatch(reg_expression, sentence)
+    if !haskey(counts, word.match)
+        counts[word.match] = 1
+    else
+        counts[word.match] += 1
+    end
+end
+
+```
+
+# References:
+(https://exercism.org/tracks/julia/exercises/word-count)
+
+```
+
+# Contributed by:- [Ihjass Thasbekha](https://github.com/Ihjass)
+"""
+function word_count(sentence)
+    lowercase_sentence = lowercase(sentence)
+    counts = Dict()
+    regex = r"(\w+('\w)?)"
+    for w in eachmatch(regex, lowercase_sentence)
+        if !haskey(counts, w.match)
+            counts[w.match] = 1
+        else
+            counts[w.match] += 1
+        end
+    end
+    return counts
+end
