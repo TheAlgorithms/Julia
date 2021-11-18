19b070a69315d7292927a0627ffeba12d08b1ea8
Ihjass Thasbekha <44056715+Ihjass@users.noreply.github.com>
2021-11-18 19:26:00 +0530

    Adding caesar.jl

---
+++ src/caesar.jl
@@ -0,0 +1,67 @@
+"""
+rotate(rot, s)
+
+Program to implement rotational cipher for the given sentence.A full description of the algorithm can be found on [wikipedia](https://en.wikipedia.org/wiki/Caesar_cipher)
+
+# Arguments:
+- `rot`: The number of rotations needed.
+- `s` : The sentence needed to rotate
+
+# Examples/Tests 
+```julia
+julia> rotate(13,"abcdefghijklmnopqrstuvwxyz")
+nopqrstuvwxyzabcdefghijklm
+
+julia> rotate(5,"omg")
+trl
+
+julia> rotate(0,"hello")
+hello
+
+```
+
+# Algorithm: 
+
+```julia
+
+if r >= 'a' && r <= 'z'
+    v = ((r - 'a') + rot) % 26
+    return v + 'a'
+end
+if r >= 'A' && r <= 'Z'
+    v = ((r - 'A') + rot) % 26
+    return v + 'A'
+end
+return r
+
+```
+
+# References:
+https://en.wikipedia.org/wiki/Caesar_cipher
+
+```
+
+# Contributed by:- [Ihjass](https://github.com/Ihjass)
+"""
+function rotate(rot, s)
+    rotate = function (r)
+        if r >= 'a' && r <= 'z'
+            v = ((r - 'a') + rot) % 26
+            return v + 'a'
+        end
+        if r >= 'A' && r <= 'Z'
+            v = ((r - 'A') + rot) % 26
+            return v + 'A'
+        end
+        return r
+    end
+    if typeof(s) == Char
+        return rotate(s)
+    end
+    result = ""
+    for r in s
+        result *= rotate(r)
+    end
+    return result
+end
+
