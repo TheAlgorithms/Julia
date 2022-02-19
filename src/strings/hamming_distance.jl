bb1c65f037067d02b30aa72d735c4e21a521e1e5
Sedat Aybars Nazlica <saybarsnazlica@gmail.com>
2022-02-19 17:41:41 +0900

    Add algorithm to find the hamming distance between two strings

---
+++ src/strings/hamming_distance.jl
@@ -0,0 +1,46 @@
+"""
+    function hamming_distance(s1::String, s2::String)
+
+In information theory, the Hamming distance between two strings of equal length is the number of positions at which the corresponding symbols are different. 
+In other words, it measures the minimum number of substitutions required to change one string into the other, or the minimum number of errors that could have transformed one string into the other. 
+In a more general context, the Hamming distance is one of several string metrics for measuring the edit distance between two sequences. It is named after the American mathematician Richard Hamming.
+
+Reference (https://en.wikipedia.org/wiki/Hamming_distance)
+
+# Arguments:
+- `s1`: First string to compare
+- `s2`: Second string to compare
+
+# Examples/Tests 
+```julia
+julia> hamming_distance("karolin", "kathrin")
+3
+
+julia> hamming_distance("0000", "1111")
+4
+
+julia> hamming_distance("ABCDEFG", "AZCDWGM")
+4
+```
+
+# Contributors:
+- [Sedat Aybars Nazlica](https://github.com/saybarsnazlica)
+"""
+
+function hamming_distance(s1::String, s2::String)
+    @assert length(s1) == length(s2)
+
+    if isempty(s1) || isempty(s2)
+        return 0
+    end
+
+    n = length(s1) 
+    distance = 0
+
+    for i in 1:n 
+        if s1[i] != s2[i]
+            distance += 1
+        end
+    end
+    return distance
+end
