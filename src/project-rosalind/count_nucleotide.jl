85bc2d1a7b6922a650e10d7b4a2cbc6700dc8314
Nikola Mircic <nikola123mircic@gmail.com>
2022-03-27 03:29:40 +0200

    fix: worng output order (#114)

--- src/project-rosalind/count_nucleotide.jl
+++ src/project-rosalind/count_nucleotide.jl
@@ -7,5 +7,5 @@ of length at most 1000 nt.
 Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s
 """
 function count_nucleotides(s::AbstractString)
-    return join(map(y -> count(x -> x == y, s), ['A', 'T', 'G', 'C']), " ")
+    return join(map(y -> count(x -> x == y, s), ['A', 'C', 'G', 'T']), " ")
 end
