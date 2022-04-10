7fc94fcdc5b8500548a82383f70db383e73052fa
Igor Malheiros <igormalheiros92@gmail.com>
2022-04-10 03:15:43 -0300

    improving tests and corner cases (#120)

--- test/longest_increasing_subsequence.jl
+++ test/longest_increasing_subsequence.jl
@@ -1,12 +1,20 @@
 @testset "Longest Increasing Subsequence" begin
     @testset "LIS: Dynamic Programming approach!" begin
         @test lis([3, 10, 2, 1, 20], Val(:dp)) == [3, 10, 20]
+        # negative elements
+        @test lis([-10, -2, -1, -20], Val(:dp)) == [-10, -2, -1]
+        # repeated elements
+        @test lis([2, 2, 3, 3], Val(:dp)) == [2, 3]
+        # one element
+        @test lis([2], Val(:dp)) == [2]
+        # empty array
+        @test lis(Int[], Val(:dp)) == Int[]
         # two possible results:
         @test lis([3, 2], Val(:dp)) in [[2], [3]]
-        @test lis([2, 2, 3], Val(:dp)) == [2, 3]
-        # two possible results
+        # two possible results:
         @test lis([50, 3, 10, 7, 40, 80], Val(:dp)) in [[3, 7, 40, 80], [3, 10, 40, 80]]
-        @test lis(Int[], Val(:dp)) == Int[]
-
+        # two possible results:
+        @test lis([3, 4, -1, 5, 8, 2, 3, 12, 7, 9, 10], Val(:dp)) in
+              [[-1, 2, 3, 7, 9, 10], [3, 4, 5, 8, 9, 10]]
     end
 end
