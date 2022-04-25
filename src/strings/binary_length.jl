3e374a1e4ca3164f511460bf3c81371292158556
F35H <MichaelFlynnFreelance@gmail.com>
2022-04-25 14:17:24 -0400

    Added new function. Renamed Variables. Other Additions

--- src/strings/binary_length.jl
+++ src/strings/binary_length.jl
@@ -1,28 +1,93 @@
-#=
-This algorithm features use of the OEIS entry A070939 - 
-  Length of Binary Representation of n.
-
-The function, as believed, is O(n) 
-=#
-
-function binLength()  
-  println("Enter Maximum Number Please")
-  maxNum = parse(UInt64, readline())
+function bin_length_right(s::AbstractString)  
+  binNum = parse(UInt, s)
   
-  mod = 0
-  len = 1
-  arr = Array{Int64}(undef, maxNum, 1)
+  finNum = 0
+  seq = 1
    
-  Threads.@threads for i in 1:maxNum
-    if (i == len)
-      mod += 1 
-      len *= 2
+  for i in 1:binNum
+    if (i == seq)
+      finNum += 1 
+      seq *= 2
     end
-      
-    arr[i] = mod
-    println(i," ",mod)
   end
 
+  return string( finNum ) 
 end
 
-exit()
+
+"""
+This algorithm features use of the OEIS entry A070939 - 
+length of Binary Representation of n. It finds 
+the length of any binary number and returns said length.
+  
+ https://oeis.org/A070939
+
+This function, as believed, is O(n)
+
+The idea follows that the sequence is dependent on 
+a repeating pattern of 2. Short for sequence, seq is the
+number of digits required before an increase in finNum 
+or final number as seen here with the first few
+iterations - i on the left, final number on the right:
+
+1 : 1 
+2 : 2
+3 : 2
+4 : 3
+5 : 3
+6 : 3
+7 : 3
+8 : 4
+cont.
+
+As you can see, for every version of i, there is an appropriate
+modified number that only repeats for the sequence of the last
+doubled amount.
+
+#Contributions:
+Contributed by F35H: https://github.com/F35H
+"""
+
+function bin_length_left(s::AbstractString)  
+  binNum = parse(UInt, s)
+  
+  finNum = 0
+  i = 1
+  
+  while i <= binNum
+    i *= 2
+     finNum += 1
+  end
+
+  return string( finNum ) 
+end
+
+"""
+This algorithm features use of the OEIS entry A070939 - 
+length of Binary Representation of n. It finds 
+the length of any binary number and returns said length.
+  
+ https://oeis.org/A070939
+
+This function, as believed, is O(n)
+
+The idea follows that the sequence is dependent on 
+a repeating pattern of 2. The final number being finNum
+increases on every doubling of i.
+
+1 : 1 
+2 : 2
+3 : 2
+4 : 3
+5 : 3
+6 : 3
+7 : 3
+8 : 4
+cont.
+
+As you can see, for every version of i, there is an appropriate
+final number that iterates on every doubling of i.
+
+#Contributions:
+Contributed by F35H: https://github.com/F35H
+"""
