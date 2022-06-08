9b01bd79dd31d759df785f9a5b062070dc890809
Nikola-Mircic <nikola123mircic@gmail.com>
2022-06-09 00:42:50 +0200

    Added tests

--- test/project-rosalind.jl
+++ test/project-rosalind.jl
@@ -12,4 +12,10 @@
     @testset "Project Rosalind: Reverse Complement" begin
         @test reverse_complement("AAAACCCGGT") == "ACCGGGTTTT"
     end
+
+    @testset "Project Rosalind: Rabbits" begin
+        @test rabbits(5, 3) == 19
+        @test rabbits(34, 3) == 574888488199
+        @test rabbits(28, 3) == 3855438727
+    end
 end
