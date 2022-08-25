a1e37a4154fc1c2d62f41a2bf0704bf9fd0192b5
yznnck <yannickbrenning2@gmail.com>
2022-08-25 19:13:15 +0200

    Added DFS and BFS: closes #138

---
+++ src/graph/bfs.jl
@@ -0,0 +1,56 @@
+using DataStructures
+
+"""
+    bfs(graph:Vector{Vector{Int}}, source::Int = 1)
+
+Depth-first search is an algorithm for traversing or searching tree or graph data structures. 
+The algorithm starts at a given vertex and explores all vertices at the present depth before moving to the next "level".
+This implementation is for educational purposes only, so it simply prints out the vertices in the order that they were traversed.
+
+# Arguments:
+- `graph`: a directed, unweighted graph
+- `source`: the source vertex from which to begin the traversal
+
+# Example
+```jldoctest
+graph = [
+    [2, 3, 6], 
+    [3, 4],
+    [4],
+    [1, 2, 5],
+    [2],
+    [1, 5]
+]
+bfs(graph, 4)
+
+# output
+
+4 1 2 5 3 6
+```
+
+Contributed by: [Yannick Brenning](https://github.com/ybrenning)
+"""
+function bfs(graph::Vector{Vector{Int}}, source::Int = 1)
+    # Use a boolean "visited" array to avoid processing a vertex more than once
+    visited = [false for _ in 1:length(graph)]
+
+    queue = Queue{Int}()
+    enqueue!(queue, source)
+
+    visited[source] = true
+
+    while length(queue) > 0
+        curr_v = dequeue!(queue)
+        print(curr_v, " ")
+
+        # Add every unvisited target to the end of the queue
+        for i in 1:length(graph[curr_v])
+            if !visited[graph[curr_v][i]]
+                enqueue!(queue, graph[curr_v][i])
+                visited[graph[curr_v][i]] = true
+            end
+        end
+    end
+
+    print("\n")
+end
---
+++ src/graph/dfs.jl
@@ -0,0 +1,56 @@
+using DataStructures
+
+"""
+    dfs(graph::Vector{Vector{Int}}, source::Int)
+
+Depth-first search is an algorithm for traversing or searching tree or graph data structures. 
+The algorithm starts at a given vertex and explores as far as possible along each branch before backtracking.
+This implementation is for educational purposes only, so it simply prints out the vertices in the order that they were traversed.
+
+# Arguments:
+- `graph`: a directed, unweighted graph
+- `source`: the source vertex from which to begin the traversal
+
+# Example
+```jldoctest
+graph = [
+    [2, 3, 6], 
+    [3, 4],
+    [4],
+    [1, 2, 5],
+    [2],
+    [1, 5]
+]
+dfs(graph, 6)
+
+# output
+
+6 5 2 4 3 1
+```
+
+Contributed by: [Yannick Brenning](https://github.com/ybrenning)
+"""
+function dfs(graph::Vector{Vector{Int}}, source::Int = 1)
+    # Use a boolean "visited" array to avoid processing a vertex more than once
+    visited = [false for _ in 1:length(graph)]
+
+    stack = Stack{Int}()
+    push!(stack, source)
+
+    visited[source] = true
+
+    while length(stack) > 0
+        curr_v = pop!(stack)
+        print(current, " ")
+
+        # Add every unvisited target to the top of the stack
+        for i in 1:length(graph[curr_v])
+            if !visited[graph[curr_v][i]]
+                push!(stack, graph[curr_v][i])
+                visited[graph[curr_v][i]] = true
+            end
+        end
+    end
+
+    print("\n")
+end
