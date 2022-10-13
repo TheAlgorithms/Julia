f8dfb3e7e7d4b05fe88d60f24f7a95398945ff23
Msninmx <72747925+Msninmx@users.noreply.github.com>
2022-10-13 04:28:35 -0500

    Added bogo sort (#149)
    * Added bogo sort
    
    * Added bogo_sort to TheAlgorithms
    
    * Added bogo_sort to TheAlgorithms
    
    * Added description to bogo_sort.jl
    
    * Added bogo_sort to tests
    
    Co-authored-by: Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>

--- src/TheAlgorithms.jl
+++ src/TheAlgorithms.jl
@@ -150,6 +150,7 @@ export jump_search
 export linear_search
 
 # Exports: sorts
+export bogo_sort!
 export bubble_sort!
 export bucket_sort!
 export counting_sort!
@@ -290,6 +291,7 @@ include("searches/jump_search.jl")
 include("searches/linear_search.jl")
 
 # Includes: sorts
+include("sorts/bogo_sort.jl")
 include("sorts/bubble_sort.jl")
 include("sorts/bucket_sort.jl")
 include("sorts/counting_sort.jl")
---
+++ src/sorts/bogo_sort.jl
@@ -0,0 +1,18 @@
+"""
+BogoSort(array)
+
+Sort an array by randomly shuffling its elements and checking if the resulting array is sorted.
+
+Bogo Sort Steps
+1. Randomly rearrange elements in array
+2. Check if sorted. If not, repeat step 1
+"""
+
+using Random
+
+function bogo_sort!(array::Vector{T}) where {T}
+  while !issorted(array)
+    Random.shuffle!(array)
+  end
+  return(array)
+end
