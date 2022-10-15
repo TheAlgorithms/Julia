b1eed025427966f43320b86290ff4f5ab2c46c19
PraneethJain <praneethjain005@gmail.com>
2022-10-15 23:14:58 +0530

    Added tests

--- test/project-euler.jl
+++ test/project-euler.jl
@@ -4,4 +4,10 @@
         @test problem_001(1000) == 233168
         @test_throws DomainError problem_001(-1)
     end
+
+    @testset "Project Euler: Problem 002" begin
+        @test problem_002(10) == 10
+        @test problem_002(4_000_000) == 4613732
+        @test_throws DomainError problem_002(-1)
+    end
 end
