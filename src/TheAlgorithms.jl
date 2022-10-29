58f2f793a8905209d94e2667b36e77370601a0d4
Praneeth Jain <praneethjain005@gmail.com>
2022-10-30 04:09:17 +0530

    Added solution to Project Euler problem 008 (#164)

--- src/TheAlgorithms.jl
+++ src/TheAlgorithms.jl
@@ -147,6 +147,7 @@ export problem_004
 export problem_005
 export problem_006
 export problem_007
+export problem_008
 
 # Exports: project-rosalind
 export count_nucleotides
@@ -295,6 +296,7 @@ include("project-euler/problem_004.jl")
 include("project-euler/problem_005.jl")
 include("project-euler/problem_006.jl")
 include("project-euler/problem_007.jl")
+include("project-euler/problem_008.jl")
 
 # Includes: project-rosalind
 include("project-rosalind/count_nucleotide.jl")
