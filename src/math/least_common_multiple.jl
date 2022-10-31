510479e29afc710a9d96132752b0e222b9a64c06
Laptop-Salad <squaretacos456@gmail.com>
2022-10-31 04:36:32 +0000

    Add least common multiple implementation

---
+++ src/math/least_common_multiple.jl
@@ -0,0 +1,32 @@
+"""
+    least_common_multiple(a::Int, b::Int)
+
+Finds the least common multiple of two integers
+
+# Arguments:
+- `a`: First of two integers
+- `b`: Second of two integers
+
+# Examples/Tests:
+```julia
+least_common_multiple(12, 76) # returns 228
+least_common_multiple(5, -10) # returns 10
+```
+
+# Reference
+- least common multiple -- https://en.wikipedia.org/wiki/Least_common_multiple 
+"""
+
+function least_common_multiple(a::Int, b::Int)
+    if a >= b
+        common_multiple = a
+    else
+        common_multiple = b
+    end
+    
+    while (common_multiple % a != 0) || (common_multiple % b != 0)
+        common_multiple += 1
+    end
+
+    return common_multiple
+end
