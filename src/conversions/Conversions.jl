ead2bddc8dd0cce80c54a7af0f587c1b57bb4006
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-12-12 13:16:12 +0800

    refactor: add Conversions submodule

---
+++ src/conversions/Conversions.jl
@@ -0,0 +1,21 @@
+"""
+  Conversions
+
+`Conversions` are conversions of measurements in Julia	
+"""
+module Conversions
+
+export celsius_to_fahrenheit
+export celsius_to_kelvin
+export fahrenheit_to_celsius
+export fahrenheit_to_kelvin
+export kelvin_to_celsius
+export kelvin_to_fahrenheit
+export weight_conversion
+export length_conversion
+
+include("weight_conversion.jl")
+include("temparature_conversion.jl")
+include("length_conversion.jl")
+
+end
