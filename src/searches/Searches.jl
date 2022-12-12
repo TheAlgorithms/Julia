9c0f378f45a068c893447273a298d370212cd22d
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-12-12 13:17:41 +0800

    refactor: add Searches submodule

---
+++ src/searches/Searches.jl
@@ -0,0 +1,19 @@
+"""
+  Searches
+
+`Searches` - search algorithms.
+"""
+module Searches
+
+export binary_search
+export exponential_search
+export interpolation_search
+export jump_search
+export linear_search
+
+include("binary_search.jl")
+include("exponential_search.jl")
+include("interpolation_search.jl")
+include("jump_search.jl")
+include("linear_search.jl")
+end
