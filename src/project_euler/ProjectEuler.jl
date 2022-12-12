e91b8d231a889dbc4700b229039ae5517ce5aa96
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-12-12 13:21:56 +0800

    refactor: add ProjectEuler submodule

---
+++ src/project_euler/ProjectEuler.jl
@@ -0,0 +1,26 @@
+"""
+  ProjectEuler
+
+`ProjectEuler` naive solutions in Julia.
+"""
+module ProjectEuler
+
+export problem_001
+export problem_002
+export problem_003
+export problem_004
+export problem_005
+export problem_006
+export problem_007
+export problem_008
+
+include("problem_001.jl")
+include("problem_002.jl")
+include("problem_003.jl")
+include("problem_004.jl")
+include("problem_005.jl")
+include("problem_006.jl")
+include("problem_007.jl")
+include("problem_008.jl")
+
+end
