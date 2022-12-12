10283ff45b9c67fc61a54218ba63dec3e2e5e4ac
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-12-12 13:17:12 +0800

    refactor: add MatrixAlgo submodule

---
+++ src/matrix/MatrixAlgo.jl
@@ -0,0 +1,19 @@
+"""
+  MatrixAlgo
+
+`MatrixAlgo` for matrix algorithms.
+"""
+module MatrixAlgo
+
+
+export determinant
+export gauss_jordan
+export lu_decompose
+export rotation_matrix
+
+include("lu_decompose.jl") # used by determinant.jl
+include("determinant.jl")
+include("gauss_jordan_elim.jl")
+include("rotation_matrix.jl")
+
+end
