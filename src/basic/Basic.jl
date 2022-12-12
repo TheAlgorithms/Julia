f624ad8760e62c0a30bdccc54c1f5c03586adb21
Soc Virnyl S. Estela <socvirnyl.estela@gmail.com>
2022-12-12 12:09:39 +0800

    refactor: create Basic submodule

---
+++ src/basic/Basic.jl
@@ -0,0 +1,16 @@
+"""
+  Basic
+
+Basic algorthims for TheAlgorithms/Julia 
+"""
+module Basic
+
+export prefix_sum
+export DifferenceArray
+export Hanoi
+
+include("hanoi.jl")
+include("difference_arr.jl")
+include("prefix_sum.jl")
+
+end
