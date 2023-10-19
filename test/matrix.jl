89377763a3ff2ecb91b28a1b6d1d99202a9d5775
vil02 <vil02@o2.pl>
2023-10-19 16:34:30 +0200

    fix: make `determinant` throw

--- test/matrix.jl
+++ test/matrix.jl
@@ -9,6 +9,9 @@ using TheAlgorithms.MatrixAlgo
         @test determinant(M1) == det(M1)
         @test round(determinant(M2), digits = 4) == round(det(M2), digits = 4)
         @test round(determinant(M3), digits = 4) == round(det(M3), digits = 4)
+
+        @test_throws DomainError determinant([1 2])
+        @test_throws DomainError determinant([1 2; 3 4; 5 6])
     end
 
     @testset "Matrix: LU Decompose" begin
