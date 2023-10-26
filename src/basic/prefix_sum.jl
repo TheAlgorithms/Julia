30797f34a0213ed28ab386679ce11687f9c5a303
SundaraRaman R <sundar.vignyaan@gmail.com>
2023-10-26 00:43:08 +0000

    Enhancements to "Basic": mainly docs  (#211)
    * docs(DifferenceArray): specify array argument's eltype as <:Number, fix spelling
    
    * refactor(DifferenceArray): explicitly use `copy` to make a copy
    
    * docs(Basic): add docstring for prefix_sum
    
    * fix(Basic): return array of proper type from prefix_sum instead of Any array
    
    * test(Basic): test prefix_sum on other Number types, not just integers

--- src/basic/prefix_sum.jl
+++ src/basic/prefix_sum.jl
@@ -1,5 +1,32 @@
+"""
+    prefix_sum(arr::Vector{<:Number})
+
+# Brief
+Given an input array of numbers, return an array of the sum of each "prefix" of the input array i.e.
+the 1st element, 1st + 2nd element, 1st + 2nd + 3rd, etc.
+
+This functionality is available in base Julia as `cumsum`.
+
+# Arguments
+- `arr`: an array of numbers
+
+# Examples
+```julia
+julia> prefix_sum([1, 2, 3])
+3-element Vector{Int64}:
+ 1
+ 3
+ 6
+
+julia> prefix_sum([0.0, 10.0, π])
+3-element Vector{Float64}:
+  0.0
+ 10.0
+ 13.141592653589793
+```
+"""
 function prefix_sum(arr::Vector{T}) where {T<:Number}
-    pre = []
+    pre = T[]
     preans = zero(T)
     for i in arr
         preans += i
